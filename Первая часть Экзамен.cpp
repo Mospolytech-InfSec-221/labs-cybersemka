7ee9c141c9f4a2a33473a0277bf29f787bd163db
cybersemka <91419637+cybersemka@users.noreply.github.com>
2023-01-19 14:34:09 +0300

    Update Первая часть Экзамен.cpp
    не правильно подписал cout

--- Первая часть Экзамен.cpp
+++ Первая часть Экзамен.cpp
@@ -1,45 +1,45 @@
-﻿#include <iostream>
-#include <vector>
+#include <iostream>
 #include <string>
-#include <cstdlib>
+#include <vector>
 
-class kishiev_kinsburg {
+class Game_Surname {
 public:
-    std::string name;
-    int gold;
-    int wood;
-    int tokens;
-    int cube1;
-    int cube2;
-    int cube3;
-    int additionalCube;
-    bool hasAnAdviser;
-    int militaryRegister;
-    int victoryPoints;
-    std::vector<std::string> buildings;
-};
+    struct kishiev_kinsburg {
+        std::string name;
+        int gold;
+        int wood;
+        int tokens;
+        int cube1;
+        int cube2;
+        int cube3;
+        int additionalCube;
+        bool hasAdviser;
+        int militaryRegister;
+        int victoryPoints;
+        std::vector<std::string> buildings;
+    };
 
-struct Adviser {
-    std::string name;
-    std::string playerName;
-};
+    struct Adviser {
+        std::string name;
+        std::string playerName;
+    };
 
-std::vector<Player> players;
-std::vector<Adviser> advisers;
-int year;
-int phase;
-std::string enemy;
+    std::vector<kishiev_kinsburg> players;
+    std::vector<Adviser> advisers;
+    int year;
+    int stage;
+    std::string enemy;
 
-void start();
-void stage1();
-void stage2();
-void stage3();
-void stage4();
-void stage5();
-void stage6();
-void stage7();
-void stage8();
-void applyBattleResults(Player& kishiev_kinsburg, int battleResult);
+    void start();
+    void stage1();
+    void stage2();
+    void stage3();
+    void stage4();
+    void stage5();
+    void stage6();
+    void stage7();
+    void stage8();
+    void applyBattleResults(kishiev_kinsburg& player, int battleResult);
 };
 
 void Game_Surname::start() {
@@ -48,28 +48,29 @@ void Game_Surname::start() {
     std::cout << "Enter the number of players: ";
     int numPlayers;
     std::cin >> numPlayers;
+
     for (int i = 0; i < numPlayers; i++) {
-        Player player;
+        kishiev_kinsburg player;
         std::cout << "Enter player " << i + 1 << "'s name: ";
-        std::cin >> kishiev_kinsburg.name;
-        kishiev_kinsburg.gold = 2;
-        kishiev_kinsburg.wood = 1;
-        kishiev_kinsburg.tokens = 1;
-        kishiev_kinsburg.cube1 = 0;
-        kishiev_kinsburg.cube2 = 0;
-        kishiev_kinsburg.cube3 = 0;
-        kishiev_kinsburg.additionalCube = 0;
-        kishiev_kinsburg.hasAdviser = false;
-        kishiev_kinsburg.militaryRegister = 0;
-        kishiev_kinsburg.victoryPoints = 0;
-        players.push_back(kishiev_kinsburg);
+        std::cin >> player.name;
+        player.gold = 2;
+        player.wood = 1;
+        player.tokens = 1;
+        player.cube1 = 0;
+        player.cube2 = 0;
+        player.cube3 = 0;
+        player.additionalCube = 0;
+        player.hasAdviser = false;
+        player.militaryRegister = 0;
+        player.victoryPoints = 0;
+        players.push_back(player);
     }
 
     year = 1;
     while (year <= 5) {
         std::cout << "Year " << year << std::endl;
-        for (phase = 1; phase <= 8; phase++) {
-            switch (phase) {
+        for (stage = 1; stage <= 8; stage++) {
+            switch (stage) {
             case 1:
                 stage1();
                 break;
@@ -99,20 +100,21 @@ void Game_Surname::start() {
         year++;
     }
 
-    std::cout << "game over!" << std::endl;
+    std::cout << "Game over!" << std::endl;
 }
 
 void Game_Surname::stage1() {
     std::cout << "stage 1: Roll dice" << std::endl;
 
     for (int i = 0; i < players.size(); i++) {
-        std::cout << "player: " << players[i].name << std::endl;
+        std::cout << "Player: " << players[i].name << std::endl;
+
         int cube1, cube2;
         std::cout << "enter player " << i + 1 << "'s dice roll: ";
         std::cin >> cube1 >> cube2;
         players[i].cube1 = cube1;
         players[i].cube2 = cube2;
-        std::cout << "Dice roll: " << players[i].cube1 << ", " << players[i].cube2 << std::endl;
+        std::cout << "dice roll: " << players[i].cube1 << ", " << players[i].cube2 << std::endl;
 
         std::cout << std::endl;
     }
@@ -120,12 +122,11 @@ void Game_Surname::stage1() {
 
 void Game_Surname::stage2() {
     std::cout << "stage 2: Take action" << std::endl;
-
     for (int i = 0; i < players.size(); i++) {
         std::cout << "player: " << players[i].name << std::endl;
 
         if (players[i].hasAdviser) {
-            std::cout << "enter the number of the action you want to take (1-5): ";
+            std::cout << "enter the number of the action you want (1,2,3,4,5): ";
             int action;
             std::cin >> action;
 
@@ -148,13 +149,13 @@ void Game_Surname::stage2() {
             }
         }
 
-        std::cout << "Resources: " << players[i].gold << " gold, " << players[i].wood << " wood, " << players[i].tokens << " tokens, " << players[i].additionalCube << " additional cube, " << players[i].militaryRegister << " military register." << std::endl;
+        std::cout << "resources: " << players[i].gold << " gold, " << players[i].wood << " wood, " << players[i].tokens << " tokens, " << players[i].additionalCube << " additional cube, " << players[i].militaryRegister << " military register." << std::endl;
         std::cout << std::endl;
     }
 }
 
 void Game_Surname::stage3() {
-    std::cout << "stage 3: build" << std::endl;
+    std::cout << "stage 3: Build" << std::endl;
     for (int i = 0; i < players.size(); i++) {
         std::cout << "player: " << players[i].name << std::endl;
 
@@ -164,23 +165,23 @@ void Game_Surname::stage3() {
 
         switch (building) {
         case 1:
-            players[i].buildings.push_back("Farm");
+            players[i].buildings.push_back("farm");
             break;
         case 2:
-            players[i].buildings.push_back("Mine");
+            players[i].buildings.push_back("mine");
             break;
         case 3:
-            players[i].buildings.push_back("Lumber Mill");
+            players[i].buildings.push_back("lumber mill");
             break;
         case 4:
-            players[i].buildings.push_back("Temple");
+            players[i].buildings.push_back("temple");
             break;
         case 5:
-            players[i].buildings.push_back("Palace");
+            players[i].buildings.push_back("palace");
             break;
         }
 
-        std::cout << "Buildings: ";
+        std::cout << "buildings: ";
         for (int j = 0; j < players[i].buildings.size(); j++) {
             std::cout << players[i].buildings[j] << " ";
         }
@@ -193,9 +194,9 @@ void Game_Surname::stage3() {
 void Game_Surname::stage4() {
     std::cout << "stage 4: Hire advisers" << std::endl;
     for (int i = 0; i < players.size(); i++) {
-        std::cout << "Player: " << players[i].name << std::endl;
+        std::cout << "player: " << players[i].name << std::endl;
 
-        std::cout << "Do you want to hire an adviser? (y/n): ";
+        std::cout << "do you want to hire adviser? (y/n): ";
         char hire;
         std::cin >> hire;
 
@@ -203,12 +204,12 @@ void Game_Surname::stage4() {
             players[i].hasAdviser = true;
 
             Adviser adviser;
-            std::cout << "Enter the name of the adviser: ";
+            std::cout << "enter the name of the adviser: ";
             std::cin >> adviser.name;
             adviser.playerName = players[i].name;
             advisers.push_back(adviser);
 
-            std::cout << "Adviser hired: " << adviser.name << std::endl;
+            std::cout << "adviser hired: " << adviser.name << std::endl;
         }
 
         std::cout << std::endl;
@@ -216,53 +217,55 @@ void Game_Surname::stage4() {
 }
 
 void Game_Surname::stage5() {
-    std::cout << "stage 5: Attack" << std::endl;
+    std::cout << "stage 5: Attack enemy" << std::endl;
     for (int i = 0; i < players.size(); i++) {
         std::cout << "player: " << players[i].name << std::endl;
 
-        std::cout << "Enter the number of tokens you want to use to attack the enemy: ";
+        std::cout << "enter the number of tokens you want to use to attack the enemy: ";
         int tokens;
         std::cin >> tokens;
-
-        players[i].tokens -= tokens;
         players[i].militaryRegister += tokens;
-
-        std::cout << "Tokens used: " << tokens << std::endl;
-        std::cout << "Military register: " << players[i].militaryRegister << std::endl;
+        players[i].tokens -= tokens;
+        std::cout << "tokens used: " << tokens << std::endl;
+        std::cout << "military register: " << players[i].militaryRegister << std::endl;
 
         std::cout << std::endl;
     }
 }
 
 void Game_Surname::stage6() {
-    std::cout << "stage 6: resources" << std::endl;
+    std::cout << "stage 6: collect resources" << std::endl;
+
     for (int i = 0; i < players.size(); i++) {
-        std::cout << "Player: " << players[i].name << std::endl;
+        std::cout << "player: " << players[i].name << std::endl;
+
         int gold = 0, wood = 0, tokens = 0;
         for (int j = 0; j < players[i].buildings.size(); j++) {
-            if (players[i].buildings[j] == "Farm") {
+            if (players[i].buildings[j] == "farm") {
                 gold++;
             }
-            else if (players[i].buildings[j] == "Mine") {
+            else if (players[i].buildings[j] == "mine") {
                 gold++;
                 tokens++;
             }
-            else if (players[i].buildings[j] == "Lumber Mill") {
+            else if (players[i].buildings[j] == "lumber mill") {
                 wood++;
             }
-            else if (players[i].buildings[j] == "Temple") {
+            else if (players[i].buildings[j] == "temple") {
                 tokens++;
             }
-            else if (players[i].buildings[j] == "Palace") {
+            else if (players[i].buildings[j] == "palace") {
                 gold++;
                 wood++;
                 tokens++;
             }
         }
+
         players[i].gold += gold;
         players[i].wood += wood;
         players[i].tokens += tokens;
-        std::cout << "Resources: " << gold << " gold, " << wood << " wood, " << tokens << " tokens." << std::endl;
+
+        std::cout << "resources: " << gold << " gold, " << wood << " wood, " << tokens << " tokens." << std::endl;
         std::cout << std::endl;
     }
 }
@@ -270,29 +273,29 @@ void Game_Surname::stage6() {
 void Game_Surname::stage7() {
     std::cout << "stage 7: End year" << std::endl;
     for (int i = 0; i < players.size(); i++) {
-        std::cout << "Player: " << players[i].name << std::endl;
+        std::cout << "player: " << players[i].name << std::endl;
         int victoryPoints = 0;
         for (int j = 0; j < players[i].buildings.size(); j++) {
-            if (players[i].buildings[j] == "Farm") {
+            if (players[i].buildings[j] == "farm") {
                 victoryPoints++;
             }
-            else if (players[i].buildings[j] == "Mine") {
+            else if (players[i].buildings[j] == "mine") {
                 victoryPoints += 2;
             }
-            else if (players[i].buildings[j] == "Lumber Mill") {
+            else if (players[i].buildings[j] == "lumber mill") {
                 victoryPoints++;
             }
-            else if (players[i].buildings[j] == "Temple") {
+            else if (players[i].buildings[j] == "temple") {
                 victoryPoints += 2;
             }
-            else if (players[i].buildings[j] == "Palace") {
+            else if (players[i].buildings[j] == "palace") {
                 victoryPoints += 3;
             }
         }
 
         players[i].victoryPoints += victoryPoints;
 
-        std::cout << "Victory points: " << players[i].victoryPoints << std::endl;
+        std::cout << "win points: " << players[i].victoryPoints << std::endl;
         std::cout << std::endl;
     }
 }
@@ -301,17 +304,20 @@ void Game_Surname::stage8() {
     std::cout << "stage 8: Battle with enemy" << std::endl;
 
     for (int i = 0; i < players.size(); i++) {
-        std::cout << "Player: " << players[i].name << std::endl;
+        std::cout << "player: " << players[i].name << std::endl;
+
         int battleResult;
-        std::cout << "Enter the battle result for player " << players[i].name << " (1-5): ";
+        std::cout << "enter the battle result for player " << players[i].name << " (1,2,3,4,5): ";
         std::cin >> battleResult;
+
         applyBattleResults(players[i], battleResult);
-        std::cout << "Resources: " << players[i].gold << " gold, " << players[i].wood << " wood, " << players[i].tokens << " tokens, " << players[i].additionalCube << " additional cube, " << players[i].militaryRegister << " military register." << std::endl;
+
+        std::cout << "resources: " << players[i].gold << " gold, " << players[i].wood << " wood, " << players[i].tokens << " tokens, " << players[i].additionalCube << " additional cube, " << players[i].militaryRegister << " military register." << std::endl;
         std::cout << std::endl;
     }
 }
 
-void Game_Surname::applyBattleResults(Player& player, int battleResult) {
+void Game_Surname::applyBattleResults(kishiev_kinsburg& player, int battleResult) {
     switch (battleResult) {
     case 1:
         player.gold += 2;
@@ -333,9 +339,9 @@ void Game_Surname::applyBattleResults(Player& player, int battleResult) {
         break;
     case 5:
         player.gold++;
-        kishiev_kinsburg.wood++;
-        kishiev_kinsburg.tokens += 2;
-        kishiev_kinsburg.additionalCube++;
+        player.wood++;
+        player.tokens += 2;
+        player.additionalCube++;
         break;
     }
 }
@@ -345,4 +351,17 @@ int main() {
     game.start();
     return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
